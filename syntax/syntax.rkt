9adbd00a1c57afeb81a1e11a5c5111ca2f47b355
erkin <me@erkin.party>
2020-01-27 11:21:32 +0300

    Add syntax.rkt

---
+++ syntax/syntax.rkt
@@ -0,0 +1,29 @@
+#lang racket/base
+
+(require racket/format
+         racket/string)
+(require syntax-color/racket-lexer)
+
+(define (lexinate results depth)
+  (define (make-span depth str)
+    (format "<span class=\"paren-~a\">~a</span>" depth str))
+  (define-values (str type paren start end) (racket-lexer (current-input-port)))
+  (if (eof-object? str)
+      (string-join (reverse results) "")
+      (case paren
+        ((\( \[ \{)
+         (lexinate (cons (make-span depth str) results) (add1 depth)))
+        ((\) \] \})
+         (lexinate (cons (make-span (sub1 depth) str) results) (sub1 depth)))
+        (else
+         (lexinate (cons str results) depth)))))
+
+(module+ main
+  (displayln "<code>")
+  (displayln (lexinate '() 0))
+  (displayln "</code>"))
+
+;; Local Variables:
+;; mode: racket
+;; eval: (rainbow-mode)
+;; End:
